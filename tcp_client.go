4557c160646990205c28614156ba81803368bc78
NoraGith <noraolo@uia.no>
2023-04-21 09:29:10 +0000

    endringer i filene

--- tcp_client.go
+++ tcp_client.go
@@ -4,15 +4,20 @@ import (
 	"net"
 	"log"
 	"os"
+	"github.com/NoraGith/is105sem03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:")
+	conn, err := net.Dial("tcp", "172.17.0.4:8001")
 	if err != nil {
 		log.Fatal(err)
 	}
     
 	log.Println("os.Args[1] = ", os.Args[1])
+
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+	log.Println("Kryptert melding: ", string(kryptertMelding))
+	_, err = conn.Write([]byte(string(kryptertMelding)))
 
  	_, err = conn.Write([]byte(os.Args[1]))
 	if err != nil {
